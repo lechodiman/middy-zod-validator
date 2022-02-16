07944f72829c30dcb5c406ecac13d59f7f13edd6
Misha Bruml <misha.bruml@kaluza.com>
2022-02-16 23:16:27 +0000

    update for upgraded dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
-import middy from '@middy/core';
-import createHttpError from 'http-errors';
-import { ZodSchema } from 'zod';
+import { MiddlewareObj } from "@middy/core";
+import createHttpError from "http-errors";
+import { ZodSchema } from "zod";
 
-export function zodValidator(
-  schema?: ZodSchema<any, any>
-): middy.MiddlewareObject<any, any> {
-  return {
-    before: async (handler, next) => {
-      if (!schema) {
-        return next();
-      }
+export const zodValidator = <T>(
+  schema: ZodSchema<T>
+): Required<Pick<MiddlewareObj<any, any>, "before">> => ({
+  before: async (request) => {
+    if (!schema) return;
 
-      try {
-        await schema.parseAsync(handler.event);
-      } catch (error) {
-        throw new createHttpError.BadRequest(error);
-      }
-
-      return next();
-    },
-  };
-}
+    try {
+      await schema.parseAsync(request.event);
+    } catch (error) {
+      throw new createHttpError.BadRequest(error as string);
+    }
+  },
+});
